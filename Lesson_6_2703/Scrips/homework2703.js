bc50bf0e58dfec47ff0369b8f3bce8e8c3ded422
vasylyeva12 <aloyna.vasilieva@gmail.com>
2023-04-02 17:48:20 +0200

    homework: filter, map, reduce, sort 27/03

--- Lesson_6_2703/Scrips/homework2703.js
+++ Lesson_6_2703/Scrips/homework2703.js
@@ -35,21 +35,55 @@ const users = [
 // 1. Сформировать новый массив, в который попадут только возраст пользователей => [28, 18, 32, 14, 54]
 const users_age = users.map (el => el.age);
 
-console.log(users_age);
+// console.log(users_age);
 
 // 2. Сформировать новый массив, в который попадет информация о каждом пользователе в формате 'Name: <firstname>, Age: <age>'
 
-const firstNameAndAge = users.map(el => el.firstname )
-console.log(firstNameAndAge);
+const {firstname, age} = users
+const firstNameAndAge = users.map(el =>  `Name: ${el.firstname}, Age: ${el.age}` )
+// console.log(firstNameAndAge);
+
 // 3. Сформировать новый массив, в который попадут только те пользователи, чье имя начинается на I
+
+const nameBeginnI = users.filter(el => el.firstname[0] ==='I')
+// console.log(nameBeginnI);
 
 
 // ***
 // 1. Сформировать новый массив, в который попадет информация о каждом совершеннолетнем пользователе в формате 'Name: <firstname>, Age: <age>'
+
+const adultUser = users
+.filter(users => users.age >= 18)
+.map(users =>  `Name: ${users.firstname}, Age: ${users.age}`)
+// console.log(adultUser);
+
 // 2. Сформировать новый массив, в который попадут только активные и совершеннолетние пользователи
+
+const activAdultUser = users.filter(users => users.age >= 18 && users.active)
+
+// console.log(activAdultUser);
 // 3. Сформировать новый массив без пользователей, чье имя начинается на букву O
+
+const newUsersO = users.filter(users => users.firstname[0] === 'O')
+// console.log(newUsersO);
+
 // 4. Сформировать новый массив, в который попадут только имена активных и совершеннолетних пользователей в нижнем регистре
+
+const adultActivUsers = users
+.filter(users => users.active && users.age >= 18)
+.map(users => users.firstname.toLowerCase())
+
+// console.log(adultActivUsers);
 
 // ****
 // 1. Сформировать новый массив, где пользователи будут отсортированы по возрасту (.sort())
+
+const usersAge = users.map(users => users.age).sort((a,b) => a - b)
+
+// console.log(usersAge);
+
 // 2. Найти сумму возрастов всех пользователей (.reduce())
+
+const sum = users.reduce((acc, el) => acc + el.age, 0)
+
+console.log(sum);
